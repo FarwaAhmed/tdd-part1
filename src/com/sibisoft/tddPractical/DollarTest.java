a2c973d690db17c6ad5007ff27ee6997279e7cb6
Farwa Ahmed <Farwa Ahmed@FarwaAhmed-PC>
2014-12-31 16:26:01 +0500

    chapter 8 - Making objects

--- src/com/sibisoft/tddPractical/DollarTest.java
+++ src/com/sibisoft/tddPractical/DollarTest.java
@@ -8,28 +8,28 @@ public class DollarTest {
 
 	@Test
 	public void testMultiplication()  {
-		Dollar ten = new Dollar(10);
-		assertEquals(new Dollar(20),ten.times(2));
-		assertEquals(new Dollar(50),ten.times(5));
+		Money ten = Money.dollar(10);
+		assertEquals(Money.dollar(20),ten.times(2));
+		assertEquals(Money.dollar(50),ten.times(5));
 		
 	}
 	
 	@Test
 	public void testEquality(){
-		assertTrue(new Dollar(10).equals(new Dollar(10)));
-		assertFalse(new Dollar(10).equals(new Dollar(20)));
+		assertTrue(Money.dollar(10).equals(Money.dollar(10)));
+		assertFalse(Money.dollar(10).equals(Money.dollar(20)));
 		
-		assertTrue(new Franc(10).equals(new Franc(10)));
-		assertFalse(new Franc(10).equals(new Franc(20)));
+		assertTrue(Money.franc(10).equals(Money.franc(10)));
+		assertFalse(Money.franc(10).equals(Money.franc(20)));
 		
-		assertFalse(new Franc(10).equals(new Dollar(10)));
+		assertFalse(Money.franc(10).equals(Money.dollar(10)));
 	}
 	
 	@Test
 	public void testFrancMultiplication()  {
-		Franc ten = new Franc(10);
-		assertEquals(new Franc(20),ten.times(2));
-		assertEquals(new Franc(50),ten.times(5));
+		Money ten = Money.franc(10);
+		assertEquals(Money.franc(20),ten.times(2));
+		assertEquals(Money.franc(50),ten.times(5));
 		
 	}
 }
--- src/com/sibisoft/tddPractical/Money.java
+++ src/com/sibisoft/tddPractical/Money.java
@@ -1,12 +1,21 @@
 package com.sibisoft.tddPractical;
 
-public class Money {
+public abstract class Money {
 
 	protected int amount;
-	
+
 	public boolean equals(Object obj){
 		Money moneyObj = (Money) obj;
 		return (amount == moneyObj.amount &&
 				this.getClass().equals(moneyObj.getClass()));
 	}
+	
+	static Money dollar(int amount){
+		return new Dollar(amount);
+	}
+	static Money franc(int amount){
+		return new Franc(amount);
+	}
+	
+	public abstract Money times(int multiplier);
 }
