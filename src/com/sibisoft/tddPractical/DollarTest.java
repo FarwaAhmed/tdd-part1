3244f23e1955f16f77b12da607f612822c390043
Farwa Ahmed <Farwa Ahmed@FarwaAhmed-PC>
2015-01-01 12:10:55 +0500

    chapter 11 - The root of all evil (sub-classes deleted)

--- src/com/sibisoft/tddPractical/DollarTest.java
+++ src/com/sibisoft/tddPractical/DollarTest.java
@@ -19,30 +19,13 @@ public class DollarTest {
 		assertTrue(Money.dollar(10).equals(Money.dollar(10)));
 		assertFalse(Money.dollar(10).equals(Money.dollar(20)));
 		
-		assertTrue(Money.franc(10).equals(Money.franc(10)));
-		assertFalse(Money.franc(10).equals(Money.franc(20)));
-		
 		assertFalse(Money.franc(10).equals(Money.dollar(10)));
 	}
 	
 	@Test
-	public void testFrancMultiplication()  {
-		Money ten = Money.franc(10);
-		assertEquals(Money.franc(20),ten.times(2));
-		assertEquals(Money.franc(50),ten.times(5));
-		
-	}
-	
-	@Test
 	public void testCurrency()  {
-		assertEquals("USD",Money.dollar(20).currency());
-		assertEquals("CHF",Money.franc(50).currency());
-	}
-	
-	@Test
-	public void testDifferentClassEquality()  {
-		assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
-		
+		assertEquals("USD",Money.dollar(20).getCurrency());
+		assertEquals("CHF",Money.franc(50).getCurrency());
 	}
 	
 }
--- src/com/sibisoft/tddPractical/Money.java
+++ src/com/sibisoft/tddPractical/Money.java
@@ -2,35 +2,48 @@ package com.sibisoft.tddPractical;
 
 public class Money {
 
+	/* Instance Variables*/
 	protected int amount;
 	protected String currency;
 	
+	/* getters/setters*/
+	public String getCurrency() {
+		return currency;
+	}
+	public int getAmount(){
+		return amount;
+	}
+	
+	/* Constructors*/
 	public Money(int amount, String currency){
 		this.amount = amount;
 		this.currency = currency;
 	}
 	
+	
+	/* Factory Methods*/
+	static Money dollar(int amount){
+		return new Money(amount,"USD");
+	}
+	static Money franc(int amount){
+		return new Money(amount,"CHF");
+	}
+	
+	/* Method to ensure equality*/
 	public boolean equals(Object obj){
 		Money moneyObj = (Money) obj;
 		return (amount == moneyObj.amount &&
-				this.currency.equals(moneyObj.currency));
+				this.getCurrency().equals(moneyObj.getCurrency()));
 	}
 	
-	static Money dollar(int amount){
-		return new Dollar(amount,"USD");
-	}
-	static Money franc(int amount){
-		return new Franc(amount,"CHF");
-	}
-	
+	/* Arithmetic operations*/
 	public Money times(int multiplier){
-		return new Money(amount * multiplier,currency);
+		return new Money(getAmount() * multiplier,getCurrency());
 	}
-	public String currency() {
-		return currency;
-	}
+	
+	/* For Debugging*/
 	public String toString(){
-		return "amount="+amount+" currency="+currency;
+		return "amount="+getAmount()+" currency="+getCurrency();
 	}
 	
 }
