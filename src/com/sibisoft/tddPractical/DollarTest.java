6498af559e0d04211b657a264b3609a62205bae7
Farwa Ahmed <Farwa Ahmed@FarwaAhmed-PC>
2015-01-03 14:10:01 +0500

    Chapter 15 - Mixed currencies addition - step 1

--- src/com/sibisoft/tddPractical/DollarTest.java
+++ src/com/sibisoft/tddPractical/DollarTest.java
@@ -7,6 +7,17 @@ import org.junit.Test;
 public class DollarTest {
 
 	@Test
+	public void testMixedAddition() {
+		// 5$ + 10 CHF = 10$ when rate is 2 : 1
+		Money fiveBucks = Money.dollar(5);
+		Money tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money reducedMoney = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+		assertEquals(Money.dollar(10), reducedMoney);
+	}
+	
+	@Test
 	public void testIdentityRate() {
 
 		assertEquals(1, new Bank().rate("USD", "USD"));
